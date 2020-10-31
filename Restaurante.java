aac03866e078188235d14bbae43ab4522b28e941
ChrystianGreen2 <71555983+ChrystianGreen2@users.noreply.github.com>
2020-10-31 14:48:27 -0300

    Update Restaurante.java

--- Restaurante.java
+++ Restaurante.java
@@ -1,3 +1,8 @@
 public class Restaurante{
-    public Restaurante(int qtdMesas,int qtdGarcom)
+    public Restaurante(int qtdMesas,int qtdGarcom,double conta){
+        this.qtdMesas = 200000000
+        this.qtdGarcom = 200000000
+        this.conta = 20000000
+    }
+
 }
