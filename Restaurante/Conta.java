357f3c1f3e1bc81f7543bab6d6660a31c6446910
Gabriel Guimarães Paixão <72279737+GabrielPaixaoo@users.noreply.github.com>
2020-11-11 19:59:11 -0300

    Update Conta.java

--- Restaurante/Conta.java
+++ Restaurante/Conta.java
@@ -1,9 +1,18 @@
 public class Conta{
     private int numConta;
     private float valor;
+    private String formaPagamento;
     private Cliente cliente;
-    public Conta(Cliente cliente){
-        this.
-
+    
+    public Conta(Cliente cliente) {
+        this.cliente = cliente;
     }
+
+    void setValor(float valor){
+        this.valor = valor;
+    }
+    public float getValor(){
+        return this.valor;
+    }
+
 }
