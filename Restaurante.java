dae5bda37b0dde03494818434ccbb7ca789cf7ed
ChrystianGreen2 <71555983+ChrystianGreen2@users.noreply.github.com>
2020-11-16 01:36:46 -0300

    Delete Restaurante.java

--- Restaurante.java
+++
@@ -1,8 +0,0 @@
-public class Restaurante{
-    public Restaurante(int qtdMesas,int qtdGarcom,double conta){
-        this.qtdMesas = 200000000
-        this.qtdGarcom = 200000000
-        this.conta = 20000000
-    }
-
-}
