134dbf6b245949ed0a613d64f8ea3fa97c1ab415
kevinkasantaroeno <71191665+kevinkasantaroeno@users.noreply.github.com>
2020-11-27 18:08:41 -0300

    Update Enfileiramento.java

--- Restaurante/Enfileiramento.java
+++ Restaurante/Enfileiramento.java
@@ -1,6 +1,6 @@
 public interface Enfileiramento
 {
     public void entrarNaFila(Cliente cliente);
-    public Cliente sairDaFila();
-    public boolean isVazio();
+    public abstract Cliente sairDaFila();
+    public abstract boolean isVazio();
 }
