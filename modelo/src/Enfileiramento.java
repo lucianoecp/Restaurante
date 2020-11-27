24971073cecb62071cfe1ba85b977c07c9944763
kevinkasantaroeno <71191665+kevinkasantaroeno@users.noreply.github.com>
2020-11-27 19:13:54 -0300

    Update Enfileiramento.java

--- modelo/src/Enfileiramento.java
+++ modelo/src/Enfileiramento.java
@@ -1,6 +1,6 @@
 public interface Enfileiramento
 {
-    public void entrarNaFila(Cliente cliente);
+    public abstract void entrarNaFila(Cliente cliente);
     public abstract Cliente sairDaFila();
     public abstract boolean isVazio();
 }
